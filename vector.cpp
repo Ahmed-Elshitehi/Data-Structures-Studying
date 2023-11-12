e6cebfcd73bc699cf262b45292fdc4d234e17413
Ahmed Salah <elshitehi1@gmail.com>
2023-11-12 12:32:07 +0200

    vector

---
+++ vector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by elshi on 11/12/2023.
+//
+
+#include "vector.h"
---
+++ vector.h
@@ -0,0 +1,16 @@
+//
+// Created by elshi on 11/12/2023.
+//
+
+#ifndef DATA_STRUCTURE_STUDYING_VECTOR_H
+#define DATA_STRUCTURE_STUDYING_VECTOR_H
+
+
+
+class vector {
+
+};
+
+
+
+#endif //DATA_STRUCTURE_STUDYING_VECTOR_H
