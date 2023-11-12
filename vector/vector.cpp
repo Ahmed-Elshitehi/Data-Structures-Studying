08d2850bdb7ca00c0ddd6bd6a092716079b14d76
Ahmed Salah <elshitehi1@gmail.com>
2023-11-12 12:38:25 +0200

    vector

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -1,52 +1,60 @@
+//
+// Created by elshi on 11/12/2023.
+//
+
+#include "vector.h"
 #include <iostream>
 using namespace std;
-class vector{
-private:
-    int *arr = nullptr;
-    int size, capacity;
-public:
-    vector(int n = 0, int v = 0) : size(max(0, n)), capacity(max(0, n)) {
-        arr = new int[capacity];
-        for (int i = 0; i < size; ++i) {
-            arr[i] = v;
-        }
-    }
 
-    void expand_capacity() {
-        capacity = capacity * 2;
-        capacity = max(capacity, 1);
-        int *new_arr = new int[capacity];
-        for (int i = 0; i < size; i++) {
-            new_arr[i] = arr[i];
-        }
-        swap(arr, new_arr);
-        delete[] new_arr;
+vector::vector(int n = 0, int v = 0) : size(max(0, n)), capacity(max(0, n)) {
+    arr = new int[capacity];
+    for (int i = 0; i < size; ++i) {
+        arr[i] = v;
     }
-    void push_back(int value){
+}
+void vector::expand_capacity() {
+    capacity = capacity * 2;
+    capacity = max(capacity, 1);
+    int *new_arr = new int[capacity];
+    for (int i = 0; i < size; i++) {
+        new_arr[i] = arr[i];
+    }
+    swap(arr, new_arr);
+    delete[] new_arr;
+}
+
+void vector::push_back(int value) {
         if (size == capacity)
             expand_capacity();
-        arr[size++] = value;
-    }
-    int at(int i) {
-        if (i >= size) {
-            return -1;
-        } else {
-            return arr[i];
-        }
-    }
-    int& operator[](int i){
+    arr[size++] = value;
+}
+
+int vector::at(int i) {
+    if (i >= size) {
+        return -1;
+    } else {
         return arr[i];
     }
-    int front(){
-        return at(0);
+}
+int &vector::operator[](int i) {
+    return arr[i];
+}
+
+const int vector::front() {
+    return arr[0];
+}
+const int vector::back() {
+    return arr[size - 1];
+}
+
+void vector::print() {
+    for (int i = 0; i < size; ++i) {
+        cout << arr[i] << ' ';
     }
-    int back(){
-        return at(size - 1);
-    }
-    void print(){
-        for (int i = 0; i < size; ++i) {
-            cout << arr[i] << ' ';
-        }
-        cout << '\n';
-    }
-};
+}
+
+vector::vector() : vector(0, 0) {
+}
+
+vector::vector(int n) : vector(n, 0) {
+}
--- vector/vector.h
+++ vector/vector.h
@@ -5,4 +5,23 @@
 #ifndef DATA_STRUCTURE_STUDYING_VECTOR_H
 #define DATA_STRUCTURE_STUDYING_VECTOR_H
 
+
+class vector {
+private:
+    int *arr = nullptr;
+    int size, capacity;
+    void expand_capacity();
+public:
+    vector(int, int);
+    vector(int);
+    vector();
+    void push_back(int value);
+    int at(int i);
+    int &operator[](int i);
+    const int front();
+    const int back();
+    void print();
+};
+
+
 #endif //DATA_STRUCTURE_STUDYING_VECTOR_H
